38b82937a9af41e6e0469cfc7d55244fe2219e0c
Javalava17 <kira.zoltan@tymestyle.com>
2020-10-29 14:58:23 -0700

    add checkboxes to other options w multiple selections ok

--- js/api-query.js
+++ js/api-query.js
@@ -10,31 +10,55 @@ function queryActivities (){
   let activity;
   let listNumber = 0;
 
-  $( "#submit" ).click(function( event ) {
+  $( "#submit" ).click(function(event) {
     event.preventDefault();
-    // $(#)
-    try {
-      let xhr = new XMLHttpRequest();
-      xhr.open('GET', 'https://www.boredapi.com/api/activity?type=recreational');
-      xhr.onreadystatechange = function () {
-          if (xhr.readyState === XMLHttpRequest.DONE) {
-          // console.log(xhr.responseText);
-  				data = JSON.parse(xhr.responseText);
-          activity = data.activity;
-          // console.log(activity);
-          listNumber = listNumber + 1;
-          $('.section-inverse').append($('<div>', {
-              class: "suggestion-class prepend",
-              style: "padding: 7px 1px 7px 17px; cursor:pointer;font-weight:300",
-              text: listNumber + ". " + activity
-            }));
-          return;
-        }
-      }
-      xhr.send(null);
+    let val1 = [];
+    let val2 = [];
+    let val3 = [];
+    let peopleQuery = '';
+    let priceQuery = '';
+    let typeQuery = '';
+        $('.people').children(':checkbox:checked').each(function(i){
+          val1[i] = $(this).val();
+        });
+        console.log(val1);
+
+        $('.price').children(':checkbox:checked').each(function(i){
+          val2[i] = $(this).val();
+        });
+        console.log(val2);
+        $('.activity-type').children(':checkbox:checked').each(function(i){
+          val3[i] = $(this).val();
+        });
+        console.log(val3);
+    if ( val1.length == 0 || val2.length == 0 || val3 == 0 ) {
+      console.log('Error!');
     }
-    catch(e) {
-      console.log('GET problem',e.message);
+    else
+    {
+      try {
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://www.boredapi.com/api/activity?type=recreational&type=cooking');
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === XMLHttpRequest.DONE) {
+            console.log(xhr.responseText);
+    				data = JSON.parse(xhr.responseText);
+            activity = data.activity;
+            // console.log(activity);
+            listNumber = listNumber + 1;
+            $('.section-inverse').append($('<div>', {
+                class: "suggestion-class prepend",
+                style: "padding: 7px 1px 7px 17px; cursor:pointer;font-weight:300",
+                text: listNumber + ". " + activity
+              }));
+            return;
+          }
+        }
+        xhr.send(null);
+      }
+      catch(e) {
+        console.log('GET problem',e.message);
+      }
     }
   });
 }
