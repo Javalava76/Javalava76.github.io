cc58dd2f8d69010da71dd8eb756d093d6d8d33c5
Javalava76 <Kira.zoltan@yahoo.com>
2025-11-10 00:07:55 -0800

    Update api-query.js
    comments and revision

--- js/api-query.js
+++ js/api-query.js
@@ -1,4 +1,4 @@
-$(function () {
+/* $(function () {
 
     queryActivities();
 
@@ -77,6 +77,77 @@ function queryActivities (){
       catch(e) {
         console.log('GET problem',e.message);
      }
+    }
+  });
+}
+ */
+
+$(function () {
+  queryActivities();
+});
+
+function queryActivities() {
+  let listNumber = 0;
+
+  $("#submit").click(function (event) {
+    event.preventDefault();
+
+    let val1 = [];
+    let val2 = [];
+    let val3 = [];
+
+    $(".people").children(":checkbox:checked").each(function (i) {
+      val1[i] = $(this).val();
+    });
+    $(".price").children(":checkbox:checked").each(function (i) {
+      val2[i] = $(this).val();
+    });
+    $(".activity-type").children(":checkbox:checked").each(function (i) {
+      val3[i] = $(this).val();
+    });
+
+    if (val1.length == 0 || val2.length == 0 || val3.length == 0) {
+      console.log("Error! Missing selections.");
+      return;
+    }
+
+    try {
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", "https://api.thecatapi.com/v1/images/search");
+
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+          const response = JSON.parse(xhr.responseText);
+          const catUrl = response[0].url;
+          console.log("Cat URL:", catUrl);
+
+          listNumber++;
+
+          // Create a new container div for each cat suggestion
+          const $newDiv = $("<div>", {
+            class: "suggestion-class prepend",
+            style: "padding: 7px 1px 7px 17px; cursor:pointer;font-weight:300;",
+          }).append(`${listNumber}. `);
+
+          // Create and append the image
+          const $catImg = $("<img>", {
+            src: catUrl,
+            alt: "Random cat",
+            style: "max-width: 400px; display: block; margin-top: 10px;",
+          });
+
+          $newDiv.append($catImg);
+          $(".section-inverse").append($newDiv);
+
+          console.log(
+            "The previous free API no longer exists so this page was updated 11/9/2025."
+          );
+        }
+      };
+
+      xhr.send();
+    } catch (e) {
+      console.log("GET problem", e.message);
     }
   });
 }
