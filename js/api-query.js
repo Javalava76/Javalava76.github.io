2413280ccf741543f6033b0054f718258f5a92fd
Javalava76 <Kira.zoltan@yahoo.com>
2025-11-09 21:34:56 -0800

    Update api-query.js

--- js/api-query.js
+++ js/api-query.js
@@ -36,13 +36,13 @@ function queryActivities (){
     }
     else
     {
-      try {
-         let xhr = new XMLHttpRequest();
+      //try {
+        // let xhr = new XMLHttpRequest();
         // xhr.open('GET', 'https://www.boredapi.com/api/activity?type=recreational&type=cooking');
-         xhr.open('GET', 'https://api.thecatapi.com/v1/images/search');
-         xhr.onreadystatechange = function () {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-            console.log(xhr.responseText);
+        // xhr.open('GET', 'https://api.thecatapi.com/v1/images/search');
+        // xhr.onreadystatechange = function () {
+         //   if (xhr.readyState === XMLHttpRequest.DONE) {
+        //    console.log(xhr.responseText);
     		//		data = JSON.parse(xhr.responseText);
         //    activity = data.activity;
             // console.log(activity);
@@ -59,14 +59,14 @@ function queryActivities (){
          console.log(`Sorry, the previous version stopped working. This page was updated 11/9/2025.`);
         //});
 
-            return;
-          }
-        }
-        xhr.send(null);
-      }
-      catch(e) {
-        console.log('GET problem',e.message);
-      }
+      //      return;
+      //    }
+      //  }
+        // xhr.send(null);
+      //}
+     // catch(e) {
+    //    console.log('GET problem',e.message);
+    //  }
     }
   });
 }
