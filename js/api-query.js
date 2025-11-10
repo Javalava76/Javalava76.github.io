9edca87531e4deb80d54b977060ee52a2c45f421
Javalava76 <Kira.zoltan@yahoo.com>
2025-11-09 21:42:17 -0800

    Update api-query.js

--- js/api-query.js
+++ js/api-query.js
@@ -39,23 +39,25 @@ function queryActivities (){
       //try {
         // let xhr = new XMLHttpRequest();
         // xhr.open('GET', 'https://www.boredapi.com/api/activity?type=recreational&type=cooking');
-        // xhr.open('GET', 'https://api.thecatapi.com/v1/images/search');
         // xhr.onreadystatechange = function () {
          //   if (xhr.readyState === XMLHttpRequest.DONE) {
         //    console.log(xhr.responseText);
     		//		data = JSON.parse(xhr.responseText);
         //    activity = data.activity;
             // console.log(activity);
+            fetch('https://api.thecatapi.com/v1/images/search')
+              .then(response => response.json())
+              .then(data => {
+              const imageUrl = data[0].url;
+            console.log(`Here's a random cat image: ${imageUrl}`);
+              });
         //    listNumber = listNumber + 1;
         //    $('.section-inverse').append($('<div>', {
         //       class: "suggestion-class prepend",
         //        style: "padding: 7px 1px 7px 17px; cursor:pointer;font-weight:300",
         //        text: listNumber + ". " + activity
         //      }));
-        // fetch('https://api.thecatapi.com/v1/images/search')
-        //   .then(response => response.json())
-        //   .then(data => {
-        // const imageUrl = data[0].url;
+       
          console.log(`Sorry, the previous version stopped working. This page was updated 11/9/2025.`);
         //});
 
