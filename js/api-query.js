9d42d4123c9eef684db7f02fdaa2dd83725e3487
Javalava76 <Kira.zoltan@yahoo.com>
2025-11-10 00:13:54 -0800

    Update api-query.js
    scrolls down to bottom of page on submit, appends new cat image each time submit button is clicked

--- js/api-query.js
+++ js/api-query.js
@@ -1,87 +1,3 @@
-/* $(function () {
-
-    queryActivities();
-
-});
-
-function queryActivities (){
-
-  let response;
-  let activity;
-  let catUrl;
-  // let catImg;
-  let listNumber = 0;
-
-  $( "#submit" ).click(function(event) {
-    event.preventDefault();
-    let val1 = [];
-    let val2 = [];
-    let val3 = [];
-    let peopleQuery = '';
-    let priceQuery = '';
-    let typeQuery = '';
-        $('.people').children(':checkbox:checked').each(function(i){
-          val1[i] = $(this).val();
-        });
-        console.log(val1);
-
-        $('.price').children(':checkbox:checked').each(function(i){
-          val2[i] = $(this).val();
-        });
-        console.log(val2);
-        $('.activity-type').children(':checkbox:checked').each(function(i){
-          val3[i] = $(this).val();
-        });
-        console.log(val3);
-    if ( val1.length == 0 || val2.length == 0 || val3.length == 0 ) {
-      console.log('Error!');
-    }
-    else
-    {
-      try {
-         let xhr = new XMLHttpRequest();
-         xhr.open('GET', 'https://api.thecatapi.com/v1/images/search');
-         xhr.onreadystatechange = function () {
-            if (xhr.readyState === XMLHttpRequest.DONE) {
-    				response = JSON.parse(xhr.responseText);
-            console.log("response:" + response);
-            catUrl = response[0].url;
-           // catImg = catUrl.src;
-           //  console.log("catUrl:", catUrl);
-           
-            listNumber = listNumber + 1;
-            $('.section-inverse').append($('<div>', {
-               class: "suggestion-class prepend",
-                style: "padding: 7px 1px 7px 17px; cursor:pointer;font-weight:300;",
-                text: listNumber + ". "
-              }));
-              $('.suggestion-class').append($('<img>', {
-               id: "catImage",
-               src: "",
-               alt: "Random cat",
-               style: "max-width: 400px; display: none;"
-              }));
-
-              let catImg = document.getElementById('catImage');
-              catImg.src = catUrl;
-              catImg.style.display = 'block';
-       
-         console.log(`The previous free API no longer exists so this page is not working as designed. This page was updated 11/9/2025.`);
-        
-
-            return;
-         }
-        }
-         xhr.send(null);
-      }
-      catch(e) {
-        console.log('GET problem',e.message);
-     }
-    }
-  });
-}
- */
-
 $(function () {
   queryActivities();
 });
@@ -106,7 +22,7 @@ function queryActivities() {
       val3[i] = $(this).val();
     });
 
-    if (val1.length == 0 || val2.length == 0 || val3.length == 0) {
+    if (val1.length === 0 || val2.length === 0 || val3.length === 0) {
       console.log("Error! Missing selections.");
       return;
     }
@@ -126,8 +42,9 @@ function queryActivities() {
           // Create a new container div for each cat suggestion
           const $newDiv = $("<div>", {
             class: "suggestion-class prepend",
-            style: "padding: 7px 1px 7px 17px; cursor:pointer;font-weight:300;",
-          }).append(`${listNumber}. `);
+            style:
+              "padding: 7px 1px 20px 17px; cursor:pointer; font-weight:300; border-bottom: 1px solid #ccc;",
+          }).append(`<strong>${listNumber}.</strong><br>`);
 
           // Create and append the image
           const $catImg = $("<img>", {
@@ -139,8 +56,16 @@ function queryActivities() {
           $newDiv.append($catImg);
           $(".section-inverse").append($newDiv);
 
+          // Smooth scroll to the bottom of the page after appending the image
+          $("html, body").animate(
+            {
+              scrollTop: $(document).height(),
+            },
+            800 // scroll duration (in ms)
+          );
+
           console.log(
-            "The previous free API no longer exists so this page was updated 11/9/2025."
+            "New cat image added and page scrolled to bottom. Updated 11/9/2025."
           );
         }
       };
