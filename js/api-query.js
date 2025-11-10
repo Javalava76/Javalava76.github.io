71edfab0af323d7a551f1c6eb179e8021a388c7a
Javalava76 <Kira.zoltan@yahoo.com>
2025-11-09 21:24:38 -0800

    Update api-query.js

--- js/api-query.js
+++ js/api-query.js
@@ -37,20 +37,28 @@ function queryActivities (){
     else
     {
       try {
-        let xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://www.boredapi.com/api/activity?type=recreational&type=cooking');
-        xhr.onreadystatechange = function () {
+         let xhr = new XMLHttpRequest();
+        // xhr.open('GET', 'https://www.boredapi.com/api/activity?type=recreational&type=cooking');
+         xhr.open('GET', 'https://api.thecatapi.com/v1/images/search');
+         xhr.onreadystatechange = function () {
             if (xhr.readyState === XMLHttpRequest.DONE) {
             console.log(xhr.responseText);
-    				data = JSON.parse(xhr.responseText);
-            activity = data.activity;
+    		//		data = JSON.parse(xhr.responseText);
+        //    activity = data.activity;
             // console.log(activity);
-            listNumber = listNumber + 1;
-            $('.section-inverse').append($('<div>', {
-                class: "suggestion-class prepend",
-                style: "padding: 7px 1px 7px 17px; cursor:pointer;font-weight:300",
-                text: listNumber + ". " + activity
-              }));
+        //    listNumber = listNumber + 1;
+        //    $('.section-inverse').append($('<div>', {
+        //       class: "suggestion-class prepend",
+        //        style: "padding: 7px 1px 7px 17px; cursor:pointer;font-weight:300",
+        //        text: listNumber + ". " + activity
+        //      }));
+        // fetch('https://api.thecatapi.com/v1/images/search')
+        //   .then(response => response.json())
+        //   .then(data => {
+        // const imageUrl = data[0].url;
+         console.log(`Sorry, the previous version stopped working. This page was updated 11/9/2025.`);
+        //});
+
             return;
           }
         }
